2a437ca0c824a729d1603eb0ac39951581770500
gladwinbobby <gladwin.bobby@gmail.com>
2016-04-27 11:52:54 +0530

    updated

--- app/src/main/java/in/codehex/arrow/MainActivity.java
+++ app/src/main/java/in/codehex/arrow/MainActivity.java
@@ -371,6 +371,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
      * Prompt the user to say something
      */
     private void promptSpeechInput() {
+        textToSpeech.stop();
         intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
